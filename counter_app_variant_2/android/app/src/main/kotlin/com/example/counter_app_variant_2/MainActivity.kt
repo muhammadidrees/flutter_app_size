5127dc751aaf9cf70115b63a2a6038c762709fdd
Muhammad Idrees <36603847+muhammadidrees@users.noreply.github.com>
2023-08-27 16:41:15 +0500

    Feat/counter app other variants (#6)
    * created a variant that only user counter package
    
    * created a variant that use button package

---
+++ counter_app_variant_2/android/app/src/main/kotlin/com/example/counter_app_variant_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.counter_app_variant_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ counter_app_variant_2/lib/counter_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CounterButton extends StatelessWidget {
+  const CounterButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    );
+  }
+}
